15d7ec804356a0c333e665e6276a95f05f3cdddb
Jeff Rose <jeff.rose@lootmarket.com>
2019-12-30 10:28:35 -0500

    Cleaned up style of event service provider.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,15 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use SocialiteProviders\Discord\DiscordExtendSocialite;
+use SocialiteProviders\GitHub\GitHubExtendSocialite;
+use SocialiteProviders\Google\GoogleExtendSocialite;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Reddit\RedditExtendSocialite;
+use SocialiteProviders\Slack\SlackExtendSocialite;
+use SocialiteProviders\Steam\SteamExtendSocialite;
+use SocialiteProviders\Twitch\TwitchExtendSocialite;
+use SocialiteProviders\Twitter\TwitterExtendSocialite;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,15 +26,15 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-            // \SocialiteProviders\Discord\DiscordExtendSocialite::class,
-            // \SocialiteProviders\GitHub\GitHubExtendSocialite::class,
-            // \SocialiteProviders\Google\GoogleExtendSocialite::class,
-            // \SocialiteProviders\Reddit\RedditExtendSocialite::class,
-            // \SocialiteProviders\Slack\SlackExtendSocialite::class,
-            // \SocialiteProviders\Steam\SteamExtendSocialite::class,
-            // \SocialiteProviders\Twitch\TwitchExtendSocialite::class,
-            // \SocialiteProviders\Twitter\TwitterExtendSocialite::class,
+        SocialiteWasCalled::class => [
+            DiscordExtendSocialite::class,
+            GitHubExtendSocialite::class,
+            GoogleExtendSocialite::class,
+            RedditExtendSocialite::class,
+            SlackExtendSocialite::class,
+            SteamExtendSocialite::class,
+            TwitchExtendSocialite::class,
+            TwitterExtendSocialite::class,
         ],
     ];
 
